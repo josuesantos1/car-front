3220e9a99090b441e3f3512253b1986f69b59aa3
Josué Santos <josue.santos.dev@gmail.com>
2022-11-08 18:45:50 -0300

    dotenv, images: domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const { parsed: myEnv } = require('dotenv').config({
+  path:'./.env.local'
+})
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_URL: process.env.API_URL
+  },
+  images: {
+    domains: ['<bucket>.s3.amazonaws.com'],
+  },
 }
 
 module.exports = nextConfig
